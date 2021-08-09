8ce66bc0cdfd5e16cc70a217b70d777f3851250d
turnerdylan <dytu4626@colorado.edu>
2021-08-09 09:46:36 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ function update() {
       var markerLocation = new Vec2(Beholder.getMarker(4).center); //vec2
       
       vectorOffset = Vec2.sub(markerStartingLocation.x, markerLocation.x);
-      debugText = Math.round(markerLocation.x * 100) / 100;
+      debugText = Math.round(300 * 100) / 100;
       //var CenterCornerDiff = new Vec2(Beholder.getMarker(4).corners[0].x - Beholder.getMarker(4).center.x, Beholder.getMarker(4).corners[0].y - Beholder.getMarker(4).center.y); //vec2
       //var markerSize = Math.sqrt( CenterCornerDiff.x*CenterCornerDiff.x + CenterCornerDiff.y*CenterCornerDiff.y ); //diff
 
